5ba2cd2fbc3d3a0c24efc6d85dfa906e51b421eb
frankito48 <fgromero2003@gmail.com>
2025-04-05 13:34:48 -0300

    DB-test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-const server = require('./src/app.js')
-const {conn} = require('./src/db.js')
+const express = require('express');
+const app = express();
 
+// Middlewares
+app.use(express.json());
 
-conn.sync({ force:false}).then(()=>{
-    server.listen(3004,()=>{
-        console.log('%s listening at 3004')
-    });
+// Ruta raíz para ver si el servidor está funcionando
+app.get('/', (req, res) => {
+  res.send('✅ Backend WebVentas funcionando correctamente');
 });
+
+// Exportar app para usar en index.js
+module.exports = app;
