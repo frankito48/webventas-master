44f1ae21d9b6c85449498e88330a8890b1a68b81
frankito48 <fgromero2003@gmail.com>
2025-04-05 12:16:32 -0300

    Usar bcryptjs para evitar errores de compilación

--- server/src/controllers/cliente/getInfoUsuario.js
+++ server/src/controllers/cliente/getInfoUsuario.js
@@ -1,5 +1,5 @@
 const { Cliente } = require('../../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const obtenerInfUsuario = async (correo, contraseña) => {
   try {
